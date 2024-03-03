5c66cdae6c37f0e577f02f4587f40793824c4d1c
Tyler Day <tmd1248@Wgmail.com>
2024-03-02 16:50:41 -0800

    prep week 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Tell me your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Tell me your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playing = true;
+        while(playing == true) {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guessCounter = 0;
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+                guessCounter += 1;
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+                guessCounter += 1;
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                guessCounter += 1;
+                Console.WriteLine("It took " + guessCounter + " tries"); 
+            }
+
+        }
+            Console.WriteLine("Would you like to play again?");
+            string answer = Console.ReadLine();
+            if(answer != "yes") {
+                playing = false;
+            }
+
+        }   
     }
 }
